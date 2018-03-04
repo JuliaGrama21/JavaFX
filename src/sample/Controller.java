f69696d06aff5c75e852934556ee7b1e46cec632
AnnChernivchan <annachernivchan@gmail.com>
2018-03-04 18:14:51 +0200

    Finish

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,57 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
-    @FXML
-    private ResourceBundle resources;
+    private ObservableList<Character> operations = FXCollections.observableArrayList('+', '-', '*', '/');
 
     @FXML
-    private URL location;
+    private TextField number1;
 
+    @FXML
+    private ComboBox<Character> operation;
+
+    @FXML
+    private TextField number2;
+
+    @FXML
+    private Button calculate;
+
+    @FXML
+    private TextField result;
 
     @FXML
     void initialize() {
+        operation.setItems(operations);
+        calculate.setOnAction(event -> {
+            Integer first = Integer.parseInt(number1.getText());
+            Integer second = Integer.parseInt(number2.getText());
+            Integer resultik = 0;
+
+            SingleSelectionModel<Character> selectionModel = operation.getSelectionModel();
+            Character selectedItem;
+            if (selectionModel == null) {
+                return;
+            }
+
+            selectedItem = selectionModel.getSelectedItem();
+
+            switch (selectedItem){
+                case '+': resultik = first + second; break;
+                case '-': resultik = first - second; break;
+                case '*': resultik = first * second; break;
+                case '/': resultik = first / second; break;
+            }
+
+            result.setText(resultik.toString());
+        });
 
     }
 
